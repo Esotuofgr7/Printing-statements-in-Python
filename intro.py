dcd6626f2b5587063a3a36ff20a7ce36081231f9
Esotuofgr7 <kachiesotu@gmail.com>
2025-07-20 23:10:30 +0100

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,11 @@
+# Printing a greeting messsage
+print("Hello, World of Coding, Humans and Nature!")
+
+# Printing my name 
+print("My name is Jeremy.")
+
+# Printing a fun fact about myself
+print("I love helping people in Life")
+
+# Performing and printing a simple calculation
+print("The result of 3 + 5 is:", 3 + 5)
